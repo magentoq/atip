531c2e8ec995d4b1623c0b48c399ad74ada4d8c9
magentoq <98724233+magentoq@users.noreply.github.com>
2022-01-30 22:12:02 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -71,7 +71,8 @@
     { key: 'input#fax', value: atipsterData.fax },
     { key: 'input#email', value: atipsterData.email },
     { key: 'input#confirmEmail', value: atipsterData.email },
-    { key: 'select#ownBehalf', value: 'Yes' }, // default
+    { key: 'select#ownBehalf', value: 'No' }, // default
+    { key:  'Are you requesting information concerning a deceased individual?', value: 'No'  },
     { key: 'select#descriptionCategory', value: atipsterData.requestor }, // requestor category
     { key: 'select#receiveMethod', value: atipsterData.delivery }
   ];
